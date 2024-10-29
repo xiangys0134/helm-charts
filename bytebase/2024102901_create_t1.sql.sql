f3b13b4a4df2930af1566dd5c2452e3ab2a6182f
unknown <you@example.com>
2024-10-29 10:50:29 +0800

    bbb

--- bytebase/2024102901_create_t1.sql.sql
+++ bytebase/2024102901_create_t1.sql.sql
@@ -1,3 +1,13 @@
+CREATE TABLE transactionhead (
+  transactionid int,
+  transactionno int,
+  cashierid int,
+  memberid int,
+  operatorid int,
+  transdate datetime
+);
+
+
 CREATE TABLE transactionhead1 (
   transactionid int,
   transactionno int,
